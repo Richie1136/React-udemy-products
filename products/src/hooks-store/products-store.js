9fcc332130a67f5f8685c2c1495da598915ef71e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-07 16:56:10 -0500

    Called my custom hook inside of index.js and im using the custom hook inside of products.js

--- products/src/hooks-store/products-store.js
+++ products/src/hooks-store/products-store.js
@@ -16,7 +16,36 @@ const configureStore = () => {
       }
       return { products: updatedProducts }
     }
-
   }
-  initStore(actions, { products })
+  initStore(actions, {
+    products: [
+      {
+        id: 'p1',
+        title: 'Red Scarf',
+        description: 'A pretty red scarf.',
+        isFavorite: false
+      },
+      {
+        id: 'p2',
+        title: 'Blue T-Shirt',
+        description: 'A pretty blue t-shirt.',
+        isFavorite: false
+      },
+      {
+        id: 'p3',
+        title: 'Green Trousers',
+        description: 'A pair of lightly green trousers.',
+        isFavorite: false
+      },
+      {
+        id: 'p4',
+        title: 'Orange Hat',
+        description: 'Street style! An orange hat.',
+        isFavorite: false
+      }
+    ]
+  })
 }
+
+
+export default configureStore
--- products/src/index.js
+++ products/src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProductsProvider from './context/products-context'
 import { BrowserRouter } from 'react-router-dom';
+import configureStore from './hooks-store/products-store';
 
 import './index.css';
 import App from './App';
 
-
+configureStore()
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
