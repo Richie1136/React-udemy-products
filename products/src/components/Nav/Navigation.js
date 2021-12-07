fbed32308602fb2302873d5d5f73ac278d0f3a29
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-07 10:28:44 -0500

    Destructing the style and children out of props

--- products/src/components/Nav/Navigation.js
+++ products/src/components/Nav/Navigation.js
@@ -9,7 +9,7 @@ const Navigation = props => {
       <nav>
         <ul>
           <li>
-            <NavLink to="/" exact>All Products</NavLink>
+            <NavLink exact to="/">All Products</NavLink>
           </li>
           <li>
             <NavLink to="/favorites">Favorites</NavLink>
---
+++ products/src/components/card/Card.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Card.css';
+
+const Card = ({ style, children }) => {
+  return <div className="card" style={style}>{children}</div>;
+};
+
+export default Card;
