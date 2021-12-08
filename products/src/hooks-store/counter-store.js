e16ce3e82f71cc6e8014c216f645f81f288ad225
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2021-12-08 09:32:56 -0500

    Added another store to add and subtract from a counter

---
+++ products/src/hooks-store/counter-store.js
@@ -0,0 +1,14 @@
+import { initStore } from "./store";
+
+
+const configureStore = () => {
+  const actions = {
+    ADD: (state, amount) => ({ counter: state.counter + amount }),
+    SUB: (state, amount) => ({ counter: state.counter - amount }),
+  };
+
+  initStore(actions, { counter: 0 })
+}
+
+
+export default configureStore
